a61a949c7d73c996b75ce2f2e23451c417244698
auroshynemobilityopc-byte <auroshynemobilityopc@gmail.com>
2026-02-18 20:28:01 +0530

    feat: Implement initial MongoDB connection and server startup logic.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const connectDB = require('./src/config/db');
 const app = require('./app');
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('MongoDB connected');
-
+connectDB().then(() => {
     app.listen(process.env.PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
     });
